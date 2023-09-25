d0632db2c395844354b00573f8c641cda05675ac
gabriel <134974960+tropacalva@users.noreply.github.com>
2023-09-25 19:57:27 -0300

    s

--- main.js
+++ main.js
@@ -12,3 +12,12 @@ function tocaSomClap () {
 
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+
