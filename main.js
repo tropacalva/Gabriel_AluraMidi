76971aacff105384abb31ddd235006402de817f8
gabriel <134974960+tropacalva@users.noreply.github.com>
2023-09-25 20:04:20 -0300

    s

--- main.js
+++ main.js
@@ -21,3 +21,56 @@ function tocaSomTim () {
 document.querySelector('.tecla_tim').onclick = tocaSomTim;
 
 
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomToim () {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+function tocaSomPsh () {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+function tocaSomTic () {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+function tocaSomTom () {
+    document.querySelector('#som_tecla_tom').play();
+}
+
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
+
+
+
+
+
+
+
+
+
+
+
+
