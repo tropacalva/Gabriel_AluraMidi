171dbf455e6d4e1fc3d8576984a2885ed5fb56fa
gabriel <134974960+tropacalva@users.noreply.github.com>
2023-10-02 19:34:18 -0300

    s

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let contador = 0;
  while (contador<listaDeTeclas.length)   {
   
  listaDeTeclas[contador].onclick = function() {
-    tocaSom('#som_tecla_pom')
+    tocaSom('#som_tecla_pom');
  }
 
  contador = contador +1;
