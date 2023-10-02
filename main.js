ca50b630862d5c97b91b675eccd0d67a06b4524d
gabriel <134974960+tropacalva@users.noreply.github.com>
2023-10-02 19:39:56 -0300

    s

--- main.js
+++ main.js
@@ -7,12 +7,19 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
  while (contador<listaDeTeclas.length)   {
+
+   const instrumento = listaDeTeclas[contador].classList[1];
+   
+   console.log(instrumento);
   
  listaDeTeclas[contador].onclick = function() {
+    
     tocaSom('#som_tecla_pom');
+
  }
 
  contador = contador +1;
+
 
  console.log(contador);
   
