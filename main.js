6fa5e07f70b9fa99b4afb7fc176a5331f8182139
gabriel <134974960+tropacalva@users.noreply.github.com>
2023-10-02 19:05:54 -0300

    s

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-
-document.querySelectorAll('.tecla')
-
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
 
